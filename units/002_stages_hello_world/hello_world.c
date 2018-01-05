03f1f026a5b9aff5796a64414d7858d545feb293
Ben Langmead <langmea@cs.jhu.edu>
2018-01-05 08:35:16 -0500

    removing outputs

--- units/002_stages_hello_world/hello_world.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-// Print "Hello world!" followed by newline and exit
-int main(void)
-{
-    puts("Hello world!");
-    return 0;
-}
--- units/002_stages_hello_world/hello_world_err.c
+++
@@ -1,6 +0,0 @@
-// Print "Hello world!" followed by newline and exit
-int main(void)
-{
-    puts("Hello world!\n");
-    return 0;
-}
