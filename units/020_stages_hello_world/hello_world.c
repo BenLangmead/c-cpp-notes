e17036044c3bbfb938c12dcf2a0982aeb09f0b03
Ben Langmead <langmea@cs.jhu.edu>
2018-04-26 14:25:08 -0400

    remove sanitizer

---
+++ units/020_stages_hello_world/hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+// Print "Hello world!" followed by newline and exit
+int main() {
+    printf("Hello world!\n");
+    return 0;
+}
---
+++ units/020_stages_hello_world/hello_world_err.c
@@ -0,0 +1,5 @@
+// Print "Hello world!" followed by newline and exit
+int main(void) {
+    printf("Hello world!\n");
+    return 0;
+}
