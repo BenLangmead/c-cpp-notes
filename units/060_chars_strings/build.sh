b7855db25b668e6061915e2fb693e792e0a1ea63
Ben Langmead <langmea@cs.jhu.edu>
2018-05-05 10:49:47 -0400

    chars_strings

---
+++ units/060_chars_strings/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+d=`pwd`
+d=`basename $d`
+
+cat >.build.sh <<EOF
+#!/bin/sh
+cd /app/$d
+make -f Makefile.slides
+EOF
+
+chmod a+x .build.sh
+
+cd ..
+docker run -t -i -v`pwd`:/app benlangmead/fedora-cpp-slides /app/$d/.build.sh
+cd $d
